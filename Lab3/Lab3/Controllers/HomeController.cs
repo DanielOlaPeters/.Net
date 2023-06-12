096470f4e4b2e3c4f2c40ac71a641fc4b8910944
DanielOlaPeters <108087906+DanielOlaPeters@users.noreply.github.com>
2023-06-12 17:26:44 -0400

    Lab3

---
+++ Lab3/Lab3/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {  
+                ViewBag.count = int.Parse(Request.Form["num"]);
+                return View();
+        
+        }
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            var properties = student.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(student);
+                if (value == null)
+                {
+                    return View("Error");
+                }
+            }
+            return View(student);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab3/Lab3/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Student
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public int studentId { get; set; }
+        [StringLength(255)]
+        [EmailAddress]
+        public String EmailAddress { get; set; }
+        [StringLength(100)]
+        [BindProperty(Name = "pass")]
+        public String Password { get; set; }
+        public String description { get; set; }
+
+    }
+}
